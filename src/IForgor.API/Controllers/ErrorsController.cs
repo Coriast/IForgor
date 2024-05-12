8473c30a03b1cea64e1b3129d2e7f0ca86f5dc60
Pedro Willian <pedrowillianpesquisador@gmail.com>
2024-05-12 13:24:04 -0300

    feat: Exception handling with ProblemDetails and ErrorsController

---
+++ src/IForgor.API/Controllers/ErrorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace IForgor.API.Controllers;
+
+public class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        return Problem(title: exception?.Message, statusCode: (int)HttpStatusCode.BadRequest);
+    }
+}
--- src/IForgor.API/Program.cs
+++ src/IForgor.API/Program.cs
@@ -1,5 +1,7 @@
+using IForgor.API.Errors;
 using IForgor.Application;
 using IForgor.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,11 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
         .AddInfrastructure(builder.Configuration);
 
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, IForgorProblemDetailsFactory>();
 }
 
 
 var app = builder.Build();
 
+app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
