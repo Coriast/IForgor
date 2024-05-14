7533452b0ffb8217cbdfe9ebd68e2b186493e2e5
Pedro Willian <pedrowillianpesquisador@gmail.com>
2024-05-14 10:51:37 -0300

    feat: Creating validator for LoginQuery

---
+++ src/IForgor.Application/Authentication/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace IForgor.Application.Authentication.Queries.Login;
+public class LoginQueryValidator : AbstractValidator<LoginQuery>
+{
+    public LoginQueryValidator()
+    {
+        RuleFor(prop => prop.Email).NotEmpty();
+        RuleFor(prop => prop.Password).NotEmpty();
+    }
+}
