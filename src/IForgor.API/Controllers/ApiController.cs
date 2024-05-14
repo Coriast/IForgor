d187c08ff33b4b094116a1ef65c4d3e60bfdf6ad
Pedro Willian <pedrowillianpesquisador@gmail.com>
2024-05-14 10:48:14 -0300

    refactor: encapsulation of Problem and ValidationProblem at ApiController

--- src/IForgor.API/Controllers/ApiController.cs
+++ src/IForgor.API/Controllers/ApiController.cs
@@ -10,23 +10,22 @@ public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors)
     {
-        if(errors.All(error => error.Type == ErrorType.Validation))
+        if (errors.Count is 0)
+            return Problem();
+
+        if (errors.All(error => error.Type == ErrorType.Validation))
         {
-            var modelStateDictionary = new ModelStateDictionary();
-
-            foreach(var error in errors)
-            {
-                modelStateDictionary.AddModelError(error.Code, error.Description);
-            }
-
-            return ValidationProblem(modelStateDictionary);
+            return ValidationProblem(errors);
         }
 
         HttpContext.Items[HttpContextItemKeys.Errors] = errors;
 
-        var firstError = errors.First();
+        return Problem(errors.First());
+    }
 
-        var statusCode = firstError.Type switch
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
         {
             ErrorType.Conflict => StatusCodes.Status409Conflict,
             ErrorType.Validation => StatusCodes.Status400BadRequest,
@@ -34,6 +33,18 @@ public class ApiController : ControllerBase
             _ => StatusCodes.Status500InternalServerError
         };
 
-        return Problem(statusCode: statusCode, title: firstError.Description);
+        return Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(error.Code, error.Description);
+        }
+
+        return ValidationProblem(modelStateDictionary);
     }
 }
